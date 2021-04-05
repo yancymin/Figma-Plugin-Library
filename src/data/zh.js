d3439ca11940a4d5203c4f3ff682453423e06a18
Yancy Min <senseslofter@gmail.com>
2021-04-05 16:16:21 +0800

    added: zh

---
+++ src/data/zh.js
@@ -0,0 +1,48 @@
+const zh = [
+  {
+    name: "Unsplash",
+    zh: "最大的高质量免费可商用图库",
+  },
+  {
+    name: "Iconify",
+    zh: "集合 60 多个图标集包含 50,000 多个矢量图标",
+  },
+  {
+    name: "Material Design Icons",
+    zh: "Material Design 官方 27,000 个图标，提供 PNG 和 SVG",
+  },
+  {
+    name: "Content Reel",
+    zh: "填充文本，图像和图标，可以自由配置数据",
+  },
+  {
+    name: "Lorem ipsum",
+    zh: "生成虚拟段落文本「英文」填充到设计中",
+  },
+  {
+    name: "Autoflow",
+    zh: "非常轻松地绘制流程图,选中两个元素自动相连",
+  },
+  {
+    name: "Figmotion",
+    zh: "带有时间线的动画制作工具",
+  },
+  {
+    name: "Vectary 3D",
+    zh: "将 Vectary 3D 中的模型渲染图片到 Figma",
+  },
+  {
+    name: "Charts",
+    zh: "生成可在 Figma 中编辑的图表",
+  },
+  {
+    name: "Blobs",
+    zh: "随机生成不规则斑点形状",
+  },
+  {
+    name: "Remove BG",
+    zh: "自动移除图像的背景,抠图神器",
+  },
+];
+
+export default zh;
